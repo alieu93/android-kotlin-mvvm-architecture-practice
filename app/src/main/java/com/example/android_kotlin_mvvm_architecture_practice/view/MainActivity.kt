f7fa0b24d7667212203711766d3bf075897ea7d2
alieu93 <adam.lieu@hotmail.com>
2021-09-25 12:40:42 -0400

    Gradle and directory setup

--- app/src/main/java/com/example/android_kotlin_mvvm_architecture_practice/view/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_mvvm_architecture_practice/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android_kotlin_mvvm_architecture_practice
+package com.example.android_kotlin_mvvm_architecture_practice.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_kotlin_mvvm_architecture_practice.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
